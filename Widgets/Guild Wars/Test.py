19d2497b1788fbf78be02e3e5194bed8e6fe3ba0
frenkey-derp <54779599+frenkey-derp@users.noreply.github.com>
2026-02-03 23:15:42 +0100

    Update Test.py

--- Widgets/Guild Wars/Test.py
+++ Widgets/Guild Wars/Test.py
@@ -1,28 +1,45 @@
-import Py4GW
+from typing import Optional
+
 
 MODULE_NAME = "Test"
+MODULE_CATEGORY = "Guild Wars"
+MODULE_ICON = "Textures\\Consumables\\Armor_of_Salvation.png"
+MODULE_TAGS = ["Testing", "Example", "Demo"]
+
+class ModuleInfo:
+    def __init__(self, name : Optional[str] = None, category : Optional[str] = None, icon : Optional[str] = None, tags : Optional[list[str]] = None):
+        self.name : Optional[str] = name
+        self.category : Optional[str] = category
+        self.icon : Optional[str] = icon
+        self.tags : Optional[list[str]] = tags
+
+MODULE_INFO = ModuleInfo(
+    name = "Test",
+    category = "Guild Wars",
+    icon = "Textures\\Consumables\\Armor_of_Salvation.png",
+    tags = ["Testing", "Example", "Demo"]
+)
+
+# def tooltip():
+#     pass
 
 def on_enable():
-    Py4GW.Console.Log(MODULE_NAME, f"{MODULE_NAME} loaded successfully.")
+    pass
 
 def on_disable():
-    Py4GW.Console.Log(MODULE_NAME, f"{MODULE_NAME} unloaded successfully.")
+    pass
     
 def configure():
-    Py4GW.Console.Log(MODULE_NAME, f"{MODULE_NAME} configuration opened.")
+    pass
     
 def draw():
-    Py4GW.Console.Log(MODULE_NAME, f"{MODULE_NAME} drawing UI.")
+    pass
     
 def update():
-    Py4GW.Console.Log(MODULE_NAME, f"{MODULE_NAME} updating state.")
+    pass
 
 def main():
-    Py4GW.Console.Log(MODULE_NAME, f"{MODULE_NAME} running.")
+    pass
     
-    
-# These functions need to be available at module level
-__all__ = ['on_enable', 'on_disable', 'configure', 'draw', 'update', 'main']
-
 if __name__ == "__main__":
     main()
