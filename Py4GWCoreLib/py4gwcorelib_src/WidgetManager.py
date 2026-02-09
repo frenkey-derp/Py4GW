cf97e31e98c637222d042093b1cda59d6b9bf289
frenkey-derp <54779599+frenkey-derp@users.noreply.github.com>
2026-02-09 10:55:28 +0100

    Update WidgetManager.py

--- Py4GWCoreLib/py4gwcorelib_src/WidgetManager.py
+++ Py4GWCoreLib/py4gwcorelib_src/WidgetManager.py
@@ -1259,15 +1259,6 @@ class Widget:
         cleaned_name = cleaned_name.replace("_", " ")
         return cleaned_name.strip()
                     
-        
-    def cleaned_name(self):
-        """Cleanup the widget name for display"""
-        ## if name starts with [0-9]-, remove that part for module cleanup and replace all _ with " "
-        import re
-        cleaned_name = re.sub(r'^\d+-', '', self.plain_name)
-        cleaned_name = cleaned_name.replace("_", " ")
-        return cleaned_name.strip()
-                    
     def __post_init__(self):
         """Extract callbacks from module after initialization"""      
         
