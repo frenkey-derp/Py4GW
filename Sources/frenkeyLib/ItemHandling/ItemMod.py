ed1139487d8619449d12844d2b007166bd95e039
frenkey-derp <54779599+frenkey-derp@users.noreply.github.com>
2026-03-01 17:21:59 +0100

    Update ItemMod.py

--- Sources/frenkeyLib/ItemHandling/ItemMod.py
+++ Sources/frenkeyLib/ItemHandling/ItemMod.py
@@ -1,3 +1,4 @@
+from Py4GWCoreLib.GlobalCache import GLOBAL_CACHE
 from Py4GWCoreLib.Item import Item
 from Sources.frenkeyLib.ItemHandling.item_modifier_parser import ItemModifierParser
 from Sources.frenkeyLib.ItemHandling.item_properties import InscriptionProperty, PrefixProperty, SuffixProperty, Upgrade
@@ -16,3 +17,39 @@ class ItemMod:
         inscription = next((i for i in properties if isinstance(i, InscriptionProperty) and i.upgrade.mod_type == ItemUpgradeType.Inscription), None)
 
         return prefix.upgrade if prefix else None, suffix.upgrade if suffix else None, inscription.upgrade if inscription else None
+    
+    @staticmethod
+    def get_item_name_without_upgrades(item_id : int) -> str:
+        item_name = GLOBAL_CACHE.Item.GetName(item_id)
+        prefix, suffix, _ = ItemMod.get_item_upgrades(item_id)
+        item_name = ItemMod.remove_upgrades_from_item_name(item_name, prefix, suffix)
+        
+        return item_name
+    
+    @staticmethod
+    def get_item_name_with_upgrades(item_id : int) -> str:
+        item_name = GLOBAL_CACHE.Item.GetName(item_id)
+        prefix, suffix, _ = ItemMod.get_item_upgrades(item_id)
+        item_name = ItemMod.apply_upgrades_to_item_name(item_name, prefix, suffix)
+        
+        return item_name
+    
+    @staticmethod
+    def apply_upgrades_to_item_name(item_name : str, prefix : Upgrade | None, suffix : Upgrade | None) -> str:
+        if prefix and hasattr(prefix, "apply_to_item_name"):
+            item_name = prefix.apply_to_item_name(item_name) # type: ignore
+            
+        if suffix and hasattr(suffix, "apply_to_item_name"):
+            item_name = suffix.apply_to_item_name(item_name) # type: ignore
+            
+        return item_name
+    
+    @staticmethod
+    def remove_upgrades_from_item_name(item_name : str, prefix : Upgrade | None, suffix : Upgrade | None) -> str:
+        if prefix and hasattr(prefix, "remove_from_item_name"):
+            item_name = prefix.remove_from_item_name(item_name) # type: ignore
+            
+        if suffix and hasattr(suffix, "remove_from_item_name"):
+            item_name = suffix.remove_from_item_name(item_name) # type: ignore
+            
+        return item_name
